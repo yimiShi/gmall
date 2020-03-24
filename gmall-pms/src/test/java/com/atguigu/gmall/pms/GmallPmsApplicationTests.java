5572bd0c8827549d7cf27edbfe41c4dc25a2ba2a
Yaming.shi <404304809@qq.com>
2020-03-24 09:12:58 +0800

    mybatis-plus逆向生成代码

--- gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
@@ -1,7 +1,12 @@
 package com.atguigu.gmall.pms;
 
+import com.atguigu.gmall.pms.entity.Brand;
+import com.atguigu.gmall.pms.entity.Product;
+import com.atguigu.gmall.pms.service.BrandService;
+import com.atguigu.gmall.pms.service.ProductService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +14,25 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class GmallPmsApplicationTests {
 
+    @Autowired
+    ProductService productService;
+
+    @Autowired
+    BrandService brandService;
+
     @Test
     public void contextLoads() {
+
+//        Product product = productService.getById("1");
+//        System.out.println(product.getName());
+
+        // 测试读写分离
+        Brand brand = new Brand();
+        brand.setName("123");
+        brandService.save(brand);
+        System.out.println("保存成功");
+
+
     }
 
 }
